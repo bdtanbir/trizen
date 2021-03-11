24799332fbc8563c0978b330c48ddbb9f92fc3bc
tanbirbd <tanbirsylhet20@gmail.com>
2021-03-11 16:21:15 +0600

    added new 2 files: archive-ts_hotel.php and loop-hotel.php file. working on hotel grid

--- archive-ts_hotel.php
+++ archive-ts_hotel.php
@@ -8,44 +8,47 @@
  */
 
 get_header();
-get_template_part('inc/breadcrumb');
+get_template_part('inc/breadcrumb-only-title');
+
 $static_infobox = get_theme_mod('show_static_infobox');
 $static_cta     = get_theme_mod('show_static_footer_cta');
 ?>
-    <section class="card-area blog-grid-wrap section--padding">
+    <section class="card-area hotel-grid-wrap section--padding">
         <div class="container">
-			<?php if(have_posts()) : ?>
+            <?php if(have_posts()) : ?>
                 <div class="row">
+
                     <div class="blog-masonry">
-						<?php
-						/* Start the Loop */
-						while ( have_posts() ) {
-							the_post();?>
+                        <?php
+                        /* Start the Loop */
+                        while ( have_posts() ) {
+                            the_post();?>
                             <div class="col-lg-4 responsive-column blog-item-isotope">
-								<?php get_template_part('template-parts/blog/blog'); ?>
+                                <?php get_template_part('template-parts/hotel/loop-hotel'); ?>
                             </div>
-						<?php } ?>
+                        <?php } ?>
                         <!-- End the Loop -->
                     </div>
+
                 </div>
 
                 <div class="row">
                     <div class="col-lg-12">
-						<?php get_template_part('template-parts/blog/pagination'); ?>
+                        <?php get_template_part('template-parts/blog/pagination'); ?>
                     </div>
                 </div>
-			<?php
-			else :
-				get_template_part( 'template-parts/content', 'none' );
-			endif;
-			?>
+            <?php
+            else :
+                get_template_part( 'template-parts/content', 'none' );
+            endif;
+            ?>
         </div>
     </section>
 
     <?php if($static_infobox == 1) { ?>
-        <div class="section-block"></div>
+    <div class="section-block"></div>
 
-        <?php
+	<?php
         get_template_part('template-parts/blog/static-infobox');
     }
 
---
+++ template-parts/hotel/loop-hotel.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+<div class="card-item">
+	<div class="card-img">
+		<a href="hotel-single.html" class="d-block">
+			<img src="images/img1.jpg" alt="hotel-img">
+		</a>
+		<span class="badge">Bestseller</span>
+		<div class="add-to-wishlist icon-element" data-toggle="tooltip" data-placement="top" title="Bookmark">
+			<i class="la la-heart-o"></i>
+		</div>
+	</div>
+	<div class="card-body">
+		<h3 class="card-title"><a href="hotel-single.html">The Millennium Hilton New York</a></h3>
+		<p class="card-meta">124 E Huron St, New york</p>
+		<div class="card-rating">
+			<span class="badge text-white">4.4/5</span>
+			<span class="review__text">Average</span>
+			<span class="rating__text">(30 Reviews)</span>
+		</div>
+		<div class="card-price d-flex align-items-center justify-content-between">
+			<p>
+				<span class="price__from">From</span>
+				<span class="price__num">$88.00</span>
+				<span class="price__text">Per night</span>
+			</p>
+			<a href="hotel-single.html" class="btn-text">See details<i class="la la-angle-right"></i></a>
+		</div>
+	</div>
+</div>
+
+
