ae12546338c220f2ab6d5b1a029f06d10026ff61
Jummanbd <tanbirsylhet20@gmail.com>
2021-08-04 19:34:52 +0600

    fixed same blog post showing on single related post. replaced content.xml file

--- functions.php
+++ functions.php
@@ -270,48 +270,6 @@ if( ! function_exists( 'trizen_auth_user_login' ) ){
 }
 
 
-
-
-/**----------------------------------
- *      One Click Demo Import
- *---------------------------------*/
-function ocdi_import_files() {
-    return array(
-        array(
-            'import_file_name'             => __('Demo Import', 'trizen'),
-            'categories'                   => 'Main',
-            'import_file_url'            => trailingslashit( get_template_directory() ) . 'demo-data/content.xml',
-            'import_widget_file_url'     => trailingslashit( get_template_directory() ) . 'demo-data/widgets.wie',
-            'import_customizer_file_url' => trailingslashit( get_template_directory() ) . 'demo-data/customizer.dat',
-            'preview_url'                  => 'https://trizen.techydevs.com/',
-        ),
-    );
-}
-// add_filter( 'pt-ocdi/pre_download_import_files', 'ocdi_import_files' );
-// add_filter( 'pt-ocdi/import_files', 'ocdi_import_files' );
-add_filter( 'ocdi/import_files', 'ocdi_import_files' );
-
-function ocdi_after_import_setup() {
-    // Assign menus to their locations.
-    $header_menu = get_term_by( 'name', 'Header Menu', 'nav_menu' );
-
-    set_theme_mod( 'nav_menu_locations', array(
-            'header_menu' => $header_menu->term_id,
-        )
-    );
-
-    // Assign front page and posts page (blog page).
-    $front_page_id = get_page_by_title( 'Home' );
-    $blog_page_id  = get_page_by_title( 'Blog' );
-
-    update_option( 'show_on_front', 'page' );
-    update_option( 'page_on_front', $front_page_id->ID );
-    update_option( 'page_for_posts', $blog_page_id->ID );
-
-}
-add_action( 'pt-ocdi/after_import', 'ocdi_after_import_setup' );
-
-
 function __search_by_title_only( $search, $wp_query ) {
 	global $wpdb;
 	if(empty($search)) {
@@ -410,4 +368,45 @@ if (!function_exists('share_hotel_room_tour_etc')) {
         <?php
     }
 }
+
+
+
+
+/**----------------------------------
+ *      One Click Demo Import
+ *---------------------------------*/
+function ocdi_import_files() {
+    return [
+        [
+            'import_file_name'             => __('Demo Import', 'trizen'),
+            'categories'                   => 'Main',
+            'local_import_file'            => trailingslashit( get_template_directory() ) . 'demo-data/content.xml',
+            'local_import_widget_file'     => trailingslashit( get_template_directory() ) . 'demo-data/widgets.wie',
+            'local_import_customizer_file' => trailingslashit( get_template_directory() ) . 'demo-data/customizer.dat',
+            'preview_url'                  => 'https://trizen.techydevs.com/',
+		],
+	];
+}
+// add_filter( 'pt-ocdi/pre_download_import_files', 'ocdi_import_files' );
+add_filter( 'ocdi/import_files', 'ocdi_import_files' );
+
+function ocdi_after_import_setup() {
+    // Assign menus to their locations.
+    $header_menu = get_term_by( 'name', 'Header Menu', 'nav_menu' );
+
+    set_theme_mod( 'nav_menu_locations', array(
+            'header_menu' => $header_menu->term_id,
+        )
+    );
+
+    // Assign front page and posts page (blog page).
+    $front_page_id = get_page_by_title( 'Home' );
+    $blog_page_id  = get_page_by_title( 'Blog' );
+
+    update_option( 'show_on_front', 'page' );
+    update_option( 'page_on_front', $front_page_id->ID );
+    update_option( 'page_for_posts', $blog_page_id->ID );
+
+}
+add_action( 'ocdi/after_import', 'ocdi_after_import_setup' );
 
--- template-parts/blog/related-blog-post.php
+++ template-parts/blog/related-blog-post.php
@@ -7,17 +7,17 @@ $related_post_pi    = explode(',', get_theme_mod('trizen_related_post_ids'));
 if(!empty(get_theme_mod('trizen_related_post_ids'))) {
 	$default_args = array(
 		'post_type'      => 'post',
-		'posts_per_page' => 2,
+		'posts_per_page' => 3,
         'post__in'       => $related_post_pi
 	);
 } else {
 	$default_args = array(
 		'post_type'      => 'post',
-		'posts_per_page' => 2
+		'posts_per_page' => 3
 	);
 }
 $query = new WP_Query($default_args);
-
+$current_single_post_id = get_the_ID(  );
 if($show_related_post == 1) {
     if($query->have_posts()) {
     ?>
@@ -30,11 +30,16 @@ if($show_related_post == 1) {
         <?php } ?>
         <div class="row pt-4">
 
-            <?php while ($query->have_posts()) { $query->the_post(); ?>
+            <?php while ($query->have_posts()) { $query->the_post(); 
+                if($current_single_post_id !== get_the_ID()) {
+                ?>
                 <div class="col-lg-6 responsive-column">
                     <?php get_template_part('template-parts/content'); ?>
                 </div>
-            <?php } ?>
+                <?php
+                }
+             } 
+            wp_reset_postdata(); ?>
 
         </div>
     </div>
